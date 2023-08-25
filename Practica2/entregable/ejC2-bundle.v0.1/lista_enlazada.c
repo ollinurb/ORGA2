22faf0a6a9f847e65844a230b4af77fb3121a587
Bruno Bruzzo <bbruzzo@dc.uba.ar>
2023-08-25 01:51:40 +0000

    lista enlazada parcial

--- Practica2/entregable/ejC2-bundle.v0.1/lista_enlazada.c
+++ Practica2/entregable/ejC2-bundle.v0.1/lista_enlazada.c
@@ -17,47 +17,61 @@ uint32_t longitud(lista_t* lista) {
 		res++;
 		it_lista++;
 	}
+	return res;
 }
 
 void agregar_al_final(lista_t* lista, uint32_t* arreglo, uint64_t longitud) {
-	uint32t* dir_cp_arreglo = malloc(sizeof(uint32t));
-	uint32t* it = arreglo;
-	while()
-	
-	uint32t* copia_dir_arreglo = arreglo;
+	uint32_t * dir_cp_arreglo = malloc(longitud * sizeof(uint32_t));
+	memcpy(dir_cp_arreglo, arreglo, longitud * sizeof(uint32_t)); //el tamaño es sizeof(uint32_t) o es longitud*sizeof(uint32_t)
 
-	if(lista_head == NULL){
-		nodo_t * nuevo_nodo = malloc(sizeof(nodo_t));
-		*nuevo_nodo = {NULL; longitud; arreglo};
-		lista_head = nuevo_nodo;
+	nodo_t * nuevo_nodo = malloc(sizeof(nodo_t));
+	nuevo_nodo->next = NULL;gi
+	nuevo_nodo->longitud = longitud;
+	nuevo_nodo->arreglo = dir_cp_arreglo;
+	if(lista->head == NULL){
+		lista->head = nuevo_nodo;
 	}else{
 		nodo_t * it_lista = lista->head;
 		while(it_lista->next != NULL){
-			it_lista++;
+			it_lista = it_lista->next;
 		}
-
-		nodo_t * nuevo_nodo = malloc(sizeof(nodo_t));
-		*nuevo_nodo = {NULL; longitud; arreglo};
-		lista_head = nuevo_nodo;
 		it_lista->next = nuevo_nodo;
 	}
 }
 
 nodo_t* iesimo(lista_t* lista, uint32_t i) {
+	nodo_t * it = lista->head;
+	for(uint32_t j = 0; j < i; j++){
+		it = it->next;
+	}
+	return it;
 }
 
 uint64_t cantidad_total_de_elementos(lista_t* lista) {
+	uint64_t res = 0;
+	nodo_t * ptr = lista->head;
+	while(ptr != NULL){
+		res = res + ptr->longitud;
+		ptr = ptr->next;
+	}
+	return res;
 }
 
 void imprimir_lista(lista_t* lista) {
+		nodo_t * ptr = lista->head;
+	while(ptr != NULL){
+		printf("| %ld | ->",ptr->longitud);
+	}
+	printf(" null\n");
 }
 
 // Función auxiliar para lista_contiene_elemento
 int array_contiene_elemento(uint32_t* array, uint64_t size_of_array, uint32_t elemento_a_buscar) {
+	return 0;
 }
 
 int lista_contiene_elemento(lista_t* lista, uint32_t elemento_a_buscar) {
-
+	return 0;
 }
 
 

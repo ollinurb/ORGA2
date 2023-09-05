51b56ae696a3c29dadb7c0a592af50867bef9a7a
Bruno Bruzzo <bruzzo.bruno@gmail.com>
2023-09-04 23:34:41 -0300

    cp4

--- 3_Assemblyx86 - Convención C/solucion/checkpoint4.asm
+++ 3_Assemblyx86 - Convención C/solucion/checkpoint4.asm
@@ -15,7 +15,9 @@ global strLen
 ; ** String **
 
 ; int32_t strCmp(char* a, char* b)
+; a[rdi] b[rsi]
 strCmp:
+
 	ret
 
 ; char* strClone(char* a)
@@ -32,6 +34,18 @@ strPrint:
 
 ; uint32_t strLen(char* a)
 strLen:
+	push rbp
+	mov rbp, rsp
+	xor rax, rax
+	cmp BYTE [rdi], 0
+	jz fin
+
+	bucle:
+	add eax, 1
+	mov rdi, [rdi + 0x1]
+	cmp BYTE rdi, 0
+	jnz bucle
+	fin:
+	pop rbp
+
 	ret
-
-

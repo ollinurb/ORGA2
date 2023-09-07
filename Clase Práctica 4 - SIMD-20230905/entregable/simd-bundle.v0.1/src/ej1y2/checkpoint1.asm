77196080ad365acef188d0bb2cc2dbdbd020ebaa
Bruno Bruzzo <bruzzo.bruno@gmail.com>
2023-09-06 21:45:10 -0300

    clean

--- Clase Práctica 4 - SIMD-20230905/entregable/simd-bundle.v0.1/src/ej1y2/checkpoint1.asm
+++
@@ -1,9 +0,0 @@
-
-section .text
-
-global invertirQW_asm
-
-; void invertirQW_asm(uint8_t* p)
-
-invertirQW_asm:
-	ret
--- Clase Práctica 4 - SIMD-20230905/entregable/simd-bundle.v0.1/src/ej1y2/checkpoint1.c
+++
@@ -1,8 +0,0 @@
-#include "checkpoints.h"
-
-void invertirBytes_c(uint64_t* p){
-    uint64_t p0 = p[0];
-	p[0] = p[1];
-    p[1] = p0;
-}
-

cb81249601a69ae8c853c12b6fce2218fbae84bd
Bruno Bruzzo <bruzzo.bruno@gmail.com>
2023-09-08 02:16:06 -0300

    checkpoint 2 idea

--- 4_SIMD/ej1y2/checkpoint2.asm
+++ 4_SIMD/ej1y2/checkpoint2.asm
@@ -1,3 +1,7 @@
+OFFSET_A EQU 0x0
+OFFSET_B EQU 0x10
+OFFSET_C EQU 0x20
+SIG_TERNA EQU 0x1C
 
 section .text
 
@@ -6,5 +10,34 @@ global checksum_asm
 ; uint8_t checksum_asm(void* array, uint32_t n)
 
 checksum_asm:
+	;prologo
+	push rbp
+	mov rbp, rsp
+
+	;cuerpo
+	xor rdx, rdx
+	xor rcx, rcx
+	xor r9, r9
+
+	pxor xmm0, xmm0
+	movq xmm0, [rdi]
+	pmovzxwd xmm0, xmm0
+	; tomamos los valores correctos A B y C. Faltaria usar SIMD para calcular. 
+	; como b y c son de 2 bytes, podemos meter 8 a la vez (IDEAL PORQUE VIENEN DE A 8)
+	; lo hacemos de 4 en 4, luego expandidos, hacemos suma, y producto por 8 (ya tendriamos la extension)
+	; esos resultados los comparamos 1 a 1 don los de C.
+
+	mov bx, [rdi + OFFSET_A] 
+	mov cx, [rdi + OFFSET_B]
+	mov r9d, [rdi + OFFSET_C]
+
+	result:
+	;epilogo
+	pop rbp
 	ret
 
+
+; 0000000000000000 0000000110100011 
+; 0000000000000000 0000000011111001 
+; 0000000000000000 0000000100010101 
+; 0000000000000000 0000000100010001

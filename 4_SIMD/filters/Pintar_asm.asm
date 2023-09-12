2b99293d021c3adead5ab8d823f5c3b302c8e7cc
Bruno Bruzzo <bruzzo.bruno@gmail.com>
2023-09-12 18:41:28 -0300

    pintar bug

--- 4_SIMD/filters/Pintar_asm.asm
+++ 4_SIMD/filters/Pintar_asm.asm
@@ -1,3 +1,15 @@
+; %define pixel_negro 0xFF
+; %define pixel_blanco 0xFFFFFFFF
+
+align 16
+section .data
+4_white_pixels times 4 db 0xFF, 0xFF, 0xFF, 0xFF
+4_black_pixels times 4 db 0x00, 0x00, 0x00, 0xFF
+; 2_black_2_white_pixels db 0x00, 0x00, 0x00, 0xFF, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF
+; 2_white_2_white_pixels db 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0xFF, 0x00, 0x00, 0x00, 0xFF
+; pixel_b dd 0xFFFFFFFF
+; pixel_n dd 0xFF000000
+; pix dw 0xFFFF
 
 global Pintar_asm
 
@@ -10,8 +22,49 @@ global Pintar_asm
 
 
 
-Pintar_asm:
+section .text
+; unsigned char* src[rdi], unsigned char* dst[rsi], int width[rdx], int height[rcx], int src_row_size[r8], int dst_row_size[r9] 
+Pintar_asm:	
+	;prologo
+	push rbp
+	mov rbp, rsp
+	;necesito un par de registros mas.
+	push rbx
+	push rbp
+	
 
+	; me copio los witdh y height para ver que copio.
+	xor rbx, rbx
+	mov rbx, rdx ; copia de height
+	xor rbp, rbp
+	mov rbp, rdx ; copia de width
+
+	pxor xmm0, xmm0
+	pxor xmm1, xmm1
+	pxor xmm2, xmm2
+	pxor xmm3, xmm3
+
+	; movdqa xmm0, [4_white_pixels]
+	; movdqa xmm1, [4_black_pixels]
+	; movdqa xmm2, [2_white_2_white_pixels]
+	; movdqa xmm3, [2_black_2_white_pixels]
+
+	
+	cmp rbx, rdx
+	jz .4_black
+	cmp rbx, rdx - 1
+	jz .4_black
+
+
+	;podria predefinirme un pixel blanco y un pixel negro?
+	;movdqa [rsi], xmm0 ;move aligned double quadword
+	.4_black:
+
+
+	;epilogo
+	pop rbp
+	pop rbx
+	pop rbp
 	ret
 	
 

d4f90778a142a61c324b72270c829d81d9eb6dc3
Bruno Bruzzo <bruzzo.bruno@gmail.com>
2023-10-08 20:03:23 -0300

    Ej1 C

--- PARCIAL 1/solucion/ej1/ej1.c
+++ PARCIAL 1/solucion/ej1/ej1.c
@@ -35,13 +35,64 @@ void listDelete(list_t* pList){
 }
 
 uint8_t contar_pagos_aprobados(list_t* pList, char* usuario){
-    return 0;
+    uint8_t res = 0;
+    listElem_t* primer = pList->first;
+    listElem_t* ultimo = pList->last;
+
+    listElem_t* it = primer;
+    while(it != NULL){
+        if(it->data->aprobado != 0 && *it->data->cobrador == *usuario){
+            res++;
+        }
+        it = it->next;    
+    }
+    return res;
 }
 
 uint8_t contar_pagos_rechazados(list_t* pList, char* usuario){
-    return 0;
+    uint8_t res = 0;
+    listElem_t* primer = pList->first;
+    listElem_t* ultimo = pList->last;
+
+    listElem_t* it = primer;
+    while(it != NULL){
+        if(it->data->aprobado == 0 && *it->data->cobrador == *usuario){
+            res++;
+        }
+        it = it->next;    
+    }
+    return res;
 }
 
 pagoSplitted_t* split_pagos_usuario(list_t* pList, char* usuario){
-    return pList;
+
+    pagoSplitted_t* res = malloc(sizeof(pagoSplitted_t)); //el espacio de memoria respuesta tiene 16 bytes.    
+    
+    uint8_t cant_ap = contar_pagos_aprobados(pList, usuario);
+    uint8_t cant_re = contar_pagos_rechazados(pList, usuario);
+
+    res->cant_aprobados = cant_ap;
+    res->cant_rechazados = cant_re;
+
+    res->aprobados = malloc(cant_ap * sizeof(pago_t**));
+    res->rechazados = malloc(cant_re * sizeof(pago_t**));
+
+    listElem_t* primer = pList->first;
+
+    listElem_t* it = primer;
+    uint8_t counterAp = 0;
+    uint8_t counterRep = 0;
+    while(it != NULL){
+        if(*it->data->cobrador == *usuario){
+            if(it->data->aprobado != 0){
+                res->aprobados[counterAp] = it->data;
+                counterAp++;
+            } else{
+                res->rechazados[counterRep] = it->data;
+                counterRep++;
+            }
+        }
+        it = it->next;
+    }
+    return res;
 }
